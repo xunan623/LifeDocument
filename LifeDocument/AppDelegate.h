c4a9c135504ce3be16898980bbc232a5c3fa50ff
xunan623 <xunan@camelot.com>
2015-12-14 11:55:17 +0800

    Initial Commit

---
+++ LifeDocument/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LifeDocument
+//
+//  Created by 许楠 on 15/12/14.
+//  Copyright © 2015年 camelot. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
